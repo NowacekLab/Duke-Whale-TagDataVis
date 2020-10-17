7dc7cf5f5b8f740d33262bb341c9a4c5b9b7d86f
890802 <hi.0802@yahoo.com>
2020-10-18 07:25:14 +0900

    Fix CSV

--- Code/csvmat.py
+++ Code/csvmat.py
@@ -4,4 +4,4 @@ name = input('Filename: ')
 data = loadmat(name)
 d = {'Heading': data['head'][:,0], 'Pitch': data['pitch'][:,0], 'Roll': data['roll'][:,0], 'Accel_X': data['Aw'][:,0], 'Accel_Y': data['Aw'][:,1], 'Accel_Z': data['Aw'][:,2]}
 df = pd.DataFrame(data=d)
-df.to_csv(r'Name.csv', index = False)
+df.to_csv(name + '.csv', index = False)
