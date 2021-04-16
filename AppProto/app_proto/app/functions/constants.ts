22b54c01a429ae192785bb7a1538c318dcc50d91
Joon Young Lee <joonlee7890@gmail.com>
2021-04-15 21:00:16 -0400

    doesnt matter

--- AppProto/app_proto/app/functions/constants.ts
+++ AppProto/app_proto/app/functions/constants.ts
@@ -1,4 +1,4 @@
-export const isDev = false;
+export const isDev = true;
 // export const isDev = false; // make false manually to test prod scripts 
 export const isWindows = process.platform === "win32";
 export const python3 = "python3";
